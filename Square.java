2ea1650526bfbf7f686a3649dab270095e949aea
2350148 <wtelsen72@gmail.com>
2021-01-06 15:30:34 +0000

    A thing

--- Square.java
+++ Square.java
@@ -1,1 +1,20 @@
+class Square {
+  private double side;
+  final MAX_SIDE_LENGTH = 10;
 
+  public square() {
+    side;
+  }
+
+  double getSide() {
+    return side;
+  }
+
+  void setSide(double theSide) {
+    side = theSide;
+  }
+
+  String toString() {
+    return "Square with side length: " + side;
+  }
+}
